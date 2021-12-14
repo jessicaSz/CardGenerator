0b155c8c9dbb3bd959cf4e7e27b7fc56bae6f074
Kaiysa <51456869+jessicaSz@users.noreply.github.com>
2021-12-14 17:52:09 +0100

    Prepared UI Manager for using buttons

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button generateButton;
+    [SerializeField] private Button saveButton;
+    [SerializeField] private Button useButton;
+
+    private CardUI cardUI;
+    private PlayerStatsUI playerStatsUI;
+
+    private void Awake()
     {
-        
+        cardUI = GetComponentInChildren<CardUI>();
+        playerStatsUI.GetComponentsInChildren<PlayerStatsUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+       generateButton.onClick.AddListener(GenerateCard);
+       saveButton.onClick.AddListener(SaveCards);
+       useButton.onClick.AddListener(UseCard);
+    }
+
+    private void OnDisable()
+    {
+        generateButton.onClick.RemoveAllListeners();
+        saveButton.onClick.RemoveAllListeners();
+        useButton.onClick.RemoveAllListeners();
+    }
+
+    private void GenerateCard()
+    {
+        GameActions.OnCardGenerate.Invoke();
+    }
+
+    private void SaveCards()
+    {
+        GameActions.OnGameSave.Invoke();
+    }
+
+    private void UseCard()
+    {
+        GameActions.OnCardUse.Invoke();
     }
 }
