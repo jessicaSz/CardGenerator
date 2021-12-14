803228c2497adb3ba3334f00382ab047f6faeefe
Kaiysa <51456869+jessicaSz@users.noreply.github.com>
2021-12-14 18:18:15 +0100

    Card access issues fixed

--- Assets/Scripts/Gameplay/Card.cs
+++ Assets/Scripts/Gameplay/Card.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class Card
 {
-    private string title;
-    private string description;
-    private Sprite picture;
-    private CardEffect cardEffect;
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public Sprite Picture { get; private set; }
+    public CardEffect CardEffect { get; private set; }
 
     public Card(string title, string description, Sprite picture, CardEffect cardEffect)
     {
-        this.title = title;
-        this.description = description;
-        this.picture = picture;
-        this.cardEffect = cardEffect;
+        this.Title = title;
+        this.Description = description;
+        this.Picture = picture;
+        this.CardEffect = cardEffect;
     }
     
 }
