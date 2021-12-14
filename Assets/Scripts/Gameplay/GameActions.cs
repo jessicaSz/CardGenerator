8c4d90424f89acbd72e6298b995443e94c1815b0
Kaiysa <51456869+jessicaSz@users.noreply.github.com>
2021-12-14 18:17:21 +0100

    Added static to game actions

--- Assets/Scripts/Gameplay/GameActions.cs
+++ Assets/Scripts/Gameplay/GameActions.cs
@@ -1,9 +1,11 @@
 using System;
 
-public class GameActions
+public static class GameActions
 {
     public static Action OnGameStart;
     public static Action OnCardGenerate;
     public static Action OnGameSave;
     public static Action OnCardUse;
+
+    public static Action<Card> OnCardGenerated;
 }
