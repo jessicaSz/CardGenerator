f6e82c2bade0bbd44b2cb70974128d61e4826894
Kaiysa <51456869+jessicaSz@users.noreply.github.com>
2021-12-14 17:50:18 +0100

    New game actions

---
+++ Assets/Scripts/Gameplay/GameActions.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class GameActions
+{
+    public static Action OnGameStart;
+    public static Action OnCardGenerate;
+    public static Action OnGameSave;
+    public static Action OnCardUse;
+}
